690431ef373fce8c9c9cbb38db53b4a92ba0df6e
tonezone6 <tonezone6@gmail.com>
2021-04-14 14:34:24 +0200

    `LoadingState` Failure type.

--- Sources/CombineExtensions/Operators/ConvertToLoadingState.swift
+++ Sources/CombineExtensions/Operators/ConvertToLoadingState.swift
@@ -4,14 +4,14 @@
 
 import Combine
 
-public enum LoadingState<Value> {
+public enum LoadingState<Value, Failure: Error> {
     case progress
     case loaded(Value)
-    case failed(Error)
+    case failed(Failure)
 }
 
 public extension Publisher {
-    func convertToLoadingState() -> AnyPublisher<LoadingState<Output>, Never> {
+    func convertToLoadingState() -> AnyPublisher<LoadingState<Output, Failure>, Never> {
         self.map(LoadingState.loaded)
             .catch { Just(.failed($0)) }
             .eraseToAnyPublisher()
