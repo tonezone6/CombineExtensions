6b8d214b60d83184993336edb40c3eaba748fe38
tonezone6 <tonezone6@gmail.com>
2021-04-16 16:37:02 +0200

    Changes.

--- Sources/CombineExtensions/Operators/ConvertToSnapshot.swift
+++ Sources/CombineExtensions/Operators/ConvertToSnapshot.swift
@@ -7,10 +7,11 @@
 
 import Combine
 
-public extension Publisher where Failure == Never {
+public extension Publisher {
 
     func convertToSnapshot<S: Section>() -> AnyPublisher<Snapshot<S>, Failure>
     where Output == [S] {
+        
         self.map { sections -> Snapshot<S> in
             var snapshot = Snapshot<S>()
             for section in sections {
