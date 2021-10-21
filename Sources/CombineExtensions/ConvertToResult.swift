2b130e3d5dda96227480abe36fc2840a15089228
tonezone6 <tonezone6@gmail.com>
2021-10-21 11:56:08 +0200

    Convert to result

---
+++ Sources/CombineExtensions/ConvertToResult.swift
@@ -0,0 +1,11 @@
+
+import Combine
+
+extension Publisher {
+    
+    public func convertToResult() -> AnyPublisher<Result<Output, Failure>, Never> {
+        self.map(Result.success)
+            .catch { Just(.failure($0)) }
+            .eraseToAnyPublisher()
+    }
+}
